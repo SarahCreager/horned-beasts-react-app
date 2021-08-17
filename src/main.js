c4626a314435a5f63070857df03cf2322f46ca17
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-08-16 21:56:28 -0700

    added src folder

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import HornedBeast from './hornedBeast';
+
+function Main() {
+  
+  return (
+  <>
+    <HornedBeast 
+    title="Pygmy Goat" 
+    imageUrl="https://i.ibb.co/802pY1p/mirciov-dan-vg3g-RVl-CYh-E-unsplash.jpg" 
+    description="I am little, but I am fierce!"
+    />
+
+    <HornedBeast 
+    title="Rhinoceros" 
+    imageUrl="https://i.ibb.co/WgWx6Ch/pauline-bernfeld-be-A3o-Bzuaa-M-unsplash.jpg" 
+    description="I may look tough, but I have a big heart!"
+    />
+
+  </>
+  );
+
+}
+
+export default Main;
